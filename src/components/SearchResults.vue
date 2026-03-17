9b66894b050c5e9b41eb1f60aaad7b09574d104f
dviset <dougieviset@gmail.com>
2026-03-17 20:46:40 +1100

    Got basic search table going from data.json

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,8 +1,38 @@
 <script setup>
-    defineProps({
+    import db from "@/data.json"
+    import { computed } from 'vue'
+    const props = defineProps({
         query: String
     })
+    let dongles = db.dongles
+    const filtered = computed(()=> {
+        return dongles.filter(d => d.barcode.toLowerCase().includes(props.query.toLowerCase()) && !d.hidden)
+    })
+
+    function goTo(bc) {
+        alert(bc)
+    } 
 </script>
 <template>
-    <P> Hello {{ query }}</P>
+    <div class="text-2xl">{{ filtered.length }} dongle<span v-show="filtered.length !== 1">s</span> found.</div>
+    <div>
+        <table class="table-fixed">
+        <thead>
+            <tr>
+                <th>Barcode</th>
+                <th>Type</th>
+                <th>Description</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="dongle in filtered" :key="dongle.barcode" :id="dongle.barcode" class="hover:bg-fuchsia-400" @click="goTo(dongle.barcode)">
+                <td>{{ dongle.barcode }}</td>
+                <td>{{ dongle.category }}</td>
+                <td>{{ dongle.description }}</td>
+                <td>{{ dongle.available }}</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
 </template>
