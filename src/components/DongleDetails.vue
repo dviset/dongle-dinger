06eaa9cf6c3a441f2d1bcca068b7581d58b01939
dviset <dougieviset@gmail.com>
2026-07-17 16:02:13 +1000

    Changed prop name from id to bc.

--- src/components/DongleDetails.vue
+++ src/components/DongleDetails.vue
@@ -1,19 +1,23 @@
 <script setup>
     import db from "@/data.json"
     import { useRoute } from "vue-router";
+    import router from "@/router.js"
+
+    
+
     let route = useRoute()
 
-    let dongle = db.dongles.find(d => d.barcode === route.params.id )
+    let dongle = db.dongles.find(d => d.barcode === route.params.bc )
     if(!dongle) {
         alert("Dongle with Barcode " + props.barcode + " not found.")
     }
-
+    alert("In DongleDetails.vue")
 </script>
 <template>
     <div class=" grid"> 
         <h1 class=" text-3xl text-center align-text-top">POOOPY {{ dongle.barcode }}</h1>
         <div class=""></div>
-        <input type="button" class=" text-white bg-gray-600 border" value="GO BACK" />
+        <input type="button" class=" text-white bg-gray-600 border" value="GO BACK" @click="router.back()" />
         <input type="button" class=" text-white bg-pink-600 border" value="CHECK OUT" />
     </div>
 </template>
